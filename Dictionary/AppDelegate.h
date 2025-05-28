6c665ea264d2d629969be0480b61bced35efc61a
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-05-28 16:26:47 +0700

    Initial Commit

---
+++ Dictionary/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Dictionary
+//
+//  Created by Luna on 2025/05/28.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Dictionary/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Dictionary
+//
+//  Created by Luna on 2025/05/28.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
